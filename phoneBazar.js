98cf5cfb157e14968079d7496f72503a66ea0d3f
ibrarahmmed <ibrarahmmedhstu@gmail.com>
2022-03-01 22:44:05 +0600

    add error and data

--- phoneBazar.js
+++ phoneBazar.js
@@ -1,3 +1,6 @@
+ document.getElementById('error-message').style.display='none';
+
+
 const searchPhone=()=>{
     const searchField=document.getElementById('search-feild');
     const searchText=searchField.value;
@@ -5,21 +8,27 @@ const searchPhone=()=>{
     // console.log(searchText);
 
     searchField.value='';
-   // document.getElementById('error-message').style.display='none';
+     document.getElementById('error-message').style.display='none';
     if(searchText==''){
         //please write some words 
+        
     }
     else{
+        
         
         fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`) 
         .then(res=>res.json())
         .then(data=>  displaySerachResult(data.data.slice(0,20)))
-        //.catch(error=>displayError(error))
+        .catch(error => displayError(error));
         
       
 
     }
    
+ }
+//  error message
+ const displayError=error=>{
+    document.getElementById('error-message').style.display='block';
  }
 
 //   display phone
@@ -40,7 +49,7 @@ const searchPhone=()=>{
         div.classList.add('col');
         div.innerHTML=`
       <div class="card h-100 w-75 shadow p-3 mb-5 bg-body rounded mx-auto">
-         <img  src="${phone.image}" class="card-img-top w-100 p-3 " alt="...">
+         <img  src="${phone.image}" class="card-img-top w-100 h-75 p-3 " alt="...">
          <div class="card-body">
              <h5 class="card-title">${phone.phone_name}</h5>
              <p class="card-text">${phone.brand}</p>
@@ -76,10 +85,14 @@ const desplayPhoneDeatail=phone=>{
     phoneDetail.textContent='';
     const div=document.createElement('div');
     div.innerHTML=`
-    <img src="${phone.data.image}" class="card-img-top w-50" alt="...">
+    <img src="${phone.data.image}" class="card-img-top w-50 m-5  " alt="...">
            <div class="card-body">
-             <h5 class="card-title">${phone.data.name}</h5>
-             <p class="card-text">${phone.data.brand}</p>
+             <h5 class="card-title">Name: ${phone.data.name}</h5>
+             <p class="card-text">Brand: ${phone.data.brand}</p>
+             <p class="card-text">Release-Date: ${phone.data.releaseDate}</p>
+             <p class="card-text">Main Features: ${phone.data.mainFeatures.sensors}</p>
+             <p class="card-text">Main Features: ${phone.data.mainFeatures.memory}</p>
+             
             
             
            </div>
