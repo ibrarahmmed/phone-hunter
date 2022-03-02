3bd3bf24f4abda0f14564f76269f013f23695675
ibrarahmmed <ibrarahmmedhstu@gmail.com>
2022-03-02 09:26:22 +0600

    add others and relase date

--- phoneBazar.js
+++ phoneBazar.js
@@ -87,28 +87,131 @@ const loadPhoneDetail = phoneId => {
 
 const desplayPhoneDeatail = phone => {
     console.log(phone)
+// other is undefine
+    if(phone.data.others===undefined){
+// release date null
+        if(phone.data.releaseDate===""){
+            const phoneDetail = document.getElementById('phone-detail');
+        phoneDetail.textContent = '';
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <img src="${phone.data.image}" class="card-img-top w-50 m-5  " alt="...">
+               <div class="card-body">
+                 <h5 class="card-title">Name: ${phone.data.name}</h5>
+                 <p class="card-text">Brand: ${phone.data.brand}</p>
+                 <p class="card-text">Release-Date: No release date found</p>
+                 <p class="card-text  ">Main Features:${phone.data.mainFeatures.sensors[0]}${phone.data.mainFeatures.sensors[1]}
+                 ${phone.data.mainFeatures.sensors[2]}${phone.data.mainFeatures.sensors[3]}${phone.data.mainFeatures.sensors[4]}
+                 ${phone.data.mainFeatures.sensors[5]}${phone.data.mainFeatures.sensors[6]}
+                 
+                 </p>
 
-    const phoneDetail = document.getElementById('phone-detail');
-    phoneDetail.textContent = '';
-    const div = document.createElement('div');
-    div.innerHTML = `
-    <img src="${phone.data.image}" class="card-img-top w-50 m-5  " alt="...">
-           <div class="card-body">
-             <h5 class="card-title">Name: ${phone.data.name}</h5>
-             <p class="card-text">Brand: ${phone.data.brand}</p>
-             <p class="card-text">Release-Date: ${phone.data.releaseDate}</p>
-             <p class="card-text  ">Main Features:${phone.data.mainFeatures.sensors[0]}${phone.data.mainFeatures.sensors[1]}
-             ${phone.data.mainFeatures.sensors[2]}${phone.data.mainFeatures.sensors[3]}${phone.data.mainFeatures.sensors[4]}
-             ${phone.data.mainFeatures.sensors[5]}${phone.data.mainFeatures.sensors[6]}
-             
-             </p>
-             <p class="card-text">Memory: ${phone.data.mainFeatures.memory}</p>
-             
-            
-            
-           </div>
-    `
-    phoneDetail.appendChild(div);
+                 <p class="card-text">Memory: ${phone.data.mainFeatures.memory}</p>
+                 
+                 
+                 
+                 
+                
+                
+               </div>
+        `
+        phoneDetail.appendChild(div);
+
+        }
+        else{
+            const phoneDetail = document.getElementById('phone-detail');
+        phoneDetail.textContent = '';
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <img src="${phone.data.image}" class="card-img-top w-50 m-5  " alt="...">
+               <div class="card-body">
+                 <h5 class="card-title">Name: ${phone.data.name}</h5>
+                 <p class="card-text">Brand: ${phone.data.brand}</p>
+                 <p class="card-text">Release-Date: ${phone.data.releaseDate}</p>
+                 <p class="card-text  ">Main Features:${phone.data.mainFeatures.sensors[0]}${phone.data.mainFeatures.sensors[1]}
+                 ${phone.data.mainFeatures.sensors[2]}${phone.data.mainFeatures.sensors[3]}${phone.data.mainFeatures.sensors[4]}
+                 ${phone.data.mainFeatures.sensors[5]}${phone.data.mainFeatures.sensors[6]}
+                 
+                 </p>
+                 <p class="card-text">Memory: ${phone.data.mainFeatures.memory}</p>
+                 
+                 
+                 
+                 
+                
+                
+               </div>
+        `
+        phoneDetail.appendChild(div);
+
+        }
+        
+    }
+
+    else {
+        if(phone.data.releaseDate===""){
+            const phoneDetail = document.getElementById('phone-detail');
+        phoneDetail.textContent = '';
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <img src="${phone.data.image}" class="card-img-top w-50 m-5  " alt="...">
+               <div class="card-body">
+                 <h5 class="card-title">Name: ${phone.data.name}</h5>
+                 <p class="card-text">Brand: ${phone.data.brand}</p>
+                 <p class="card-text">Release-Date: No release date found</p>
+                 <p class="card-text  ">Main Features:${phone.data.mainFeatures.sensors[0]}${phone.data.mainFeatures.sensors[1]}
+                 ${phone.data.mainFeatures.sensors[2]}${phone.data.mainFeatures.sensors[3]}${phone.data.mainFeatures.sensors[4]}
+                 ${phone.data.mainFeatures.sensors[5]}${phone.data.mainFeatures.sensors[6]}
+                 
+                 </p>
+                 <p class="card-text">Memory: ${phone.data.mainFeatures.memory}</p>
+                 <p class="card-text">Others: WLAN:${phone.data.others.WlAN} GPS:${phone.data.others.GPS} NFC:${phone.data.others.NFC}
+                 Radio:${phone.data.others.Radio}
+                 </p>
+
+
+                 
+                 
+                 
+                 
+                
+                
+               </div>
+        `
+        phoneDetail.appendChild(div);
+
+        }
+        else{
+            const phoneDetail = document.getElementById('phone-detail');
+        phoneDetail.textContent = '';
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <img src="${phone.data.image}" class="card-img-top w-50 m-5  " alt="...">
+               <div class="card-body">
+                 <h5 class="card-title">Name: ${phone.data.name}</h5>
+                 <p class="card-text">Brand: ${phone.data.brand}</p>
+                 <p class="card-text">Release-Date: ${phone.data.releaseDate}</p>
+                 <p class="card-text  ">Main Features:${phone.data.mainFeatures.sensors[0]}${phone.data.mainFeatures.sensors[1]}
+                 ${phone.data.mainFeatures.sensors[2]}${phone.data.mainFeatures.sensors[3]}${phone.data.mainFeatures.sensors[4]}
+                 ${phone.data.mainFeatures.sensors[5]}${phone.data.mainFeatures.sensors[6]}
+                 
+                 </p>
+                 <p class="card-text">Memory: ${phone.data.mainFeatures.memory}</p>
+                 <p class="card-text">Others: WLAN:${phone.data.others.WlAN} GPS:${phone.data.others.GPS} NFC:${phone.data.others.NFC}
+                 Radio:${phone.data.others.Radio}
+                 </p>
+                 
+                 
+                 
+                 
+                
+                
+               </div>
+        `
+        phoneDetail.appendChild(div);
+
+        }
+    }
 
 }
 
